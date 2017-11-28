cf563d787493339f981504a151c9b1452f108c23
tomj <tarjohnson1979@gmail.com>
2017-11-28 15:27:32 +0000

    Start test-driving collision detection algorithms

--- src/Shape.hs
+++ src/Shape.hs
@@ -1,6 +1,11 @@
 module Shape where
 
+infixl 2 !!!
+
 data Point = Point { x :: Float, y :: Float } deriving (Show, Eq)
 
 data Shape = Rectangle { left :: Float, right :: Float, top :: Float, bottom :: Float }
            | Circle { centre :: Point, radius :: Float } deriving (Show, Eq)
+
+(!!!) :: Shape -> Shape -> Bool
+a@Rectangle {} !!! b@Rectangle {} = not (top a < bottom b || top b < bottom a)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,18 @@
 module Main where
 
 import Test.HUnit
+import Shape
 
 tests = TestList [
-  TestLabel "five is greater than three"
-  $ (5 > 3) ~=? True,
+  TestLabel "Any overlap means there's a collision"
+  $ TestCase (let rectA = Rectangle { left = 10, right = 30, top = 10, bottom = -10 }
+                  rectB = Rectangle { left = 10, right = 30, top = 30, bottom = 5 }
+               in (rectA !!! rectB) @=? True)
 
-  TestLabel "two is greater than seven"
-  $ (2 > 7) ~=? True
-
+  , TestLabel "A gap below first rectangle means no collision"
+  $ TestCase (let rectA = Rectangle { left = 10, right = 30, top = 10, bottom = -10 }
+                  rectB = Rectangle { left = 10, right = 30, top = 30, bottom = 15 }
+               in (rectA !!! rectB) @=? False)
   ]
 
 main :: IO ()
